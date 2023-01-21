7e7eed677180c0b33c20dbb49d29d5db7d1b4795
tratchapong <tratchapong@gmail.com>
2023-01-22 00:06:35 +0700

    Basic pattern of v6

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", element: <h1>Hello, Homepage</h1> },
+  { path: "/products", element: <h1>Hello, Products page</h1> },
+  { path: "/services", element: <h1>Hello, Service page</h1> },
+  { path: "/about", element: <h1>Hello, About page</h1> },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <a href="/services">Services</a>
+        <a href="/about">About</a>
+      </nav>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/routes/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Root() {
+  return (
+    <div>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/products">Products</a>
+        <a href="/services">Services</a>
+        <a href="/about">About</a>
+      </nav>
+    </div>
+  )
+}
+
+export default Root
